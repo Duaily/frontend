4ae420458e65ea552d42b126456bf69fdfd299e8
sangminlee98 <tkdals0975@naver.com>
2022-11-14 22:15:06 +0900

    fix : 메인페이지 전체적인 폰트크기 & 카드리스트 UI 수정

--- src/components/HouseCard/index.tsx
+++ src/components/HouseCard/index.tsx
@@ -18,19 +18,22 @@ interface HouseCardProps {
 function HouseCard(props: HouseCardProps) {
   return (
     <CardContainer whileHover={{ scale: 1.01, opacity: 0.8 }}>
-      <CardImage img_src={props.houseImage} />
-      <CardTitle>{props.title}</CardTitle>
+      <CardImage img_src={props.houseImage}>
+        <CardStatus>입주 가능</CardStatus>
+      </CardImage>
       <CardInfoBox>
-        <CardStatus>입주가능</CardStatus>
+        <CardTitle>{props.title}</CardTitle>
         <CardInfo>
           <p>위치 : {props.location}</p>
           <p>
             가격 : {processCount(props.min_price)}원 ~{" "}
             {processCount(props.max_price)}원
           </p>
+        </CardInfo>
+        <CardMetaData>
           <p>업로드 일자 : {dayjs(props.upload_date).format("YYYY.MM.DD")}</p>
           <p>작성자 : {props.author}</p>
-        </CardInfo>
+        </CardMetaData>
       </CardInfoBox>
     </CardContainer>
   );
@@ -41,49 +44,65 @@ export default HouseCard;
 const CardContainer = styled(motion.div)`
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
   align-items: flex-start;
-  width: 580px;
-  height: 625px;
+  width: 100%;
+  height: 400px;
+  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.25);
+  border-radius: 5px;
+  cursor: pointer;
 `;
 const CardImage = styled.div<{ img_src: string }>`
+  position: relative;
   width: 100%;
-  height: 390px;
+  height: 220px;
   background-image: url(${(props) => props.img_src});
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
   background-color: white;
-`;
-const CardTitle = styled.h1`
-  font-size: 30px;
-  font-weight: bold;
-  white-space: pre-wrap;
-  word-break: break-all;
-`;
-const CardInfoBox = styled.div`
-  display: flex;
-  height: 115px;
+  border-radius: 5px 5px 0 0;
 `;
 const CardStatus = styled.div`
   display: flex;
   justify-content: center;
   align-items: center;
-  width: 70px;
-  height: 100%;
-  margin-right: 30px;
-  padding: 28px 16px;
+  position: absolute;
+  top: 15px;
+  right: 15px;
+  width: 20%;
+  height: 30px;
   background-color: ${(props) => props.theme.orange_color};
   color: white;
+  font-size: 14px;
+  font-weight: bold;
+`;
+const CardInfoBox = styled.div`
+  flex: 1;
+  width: 100%;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+`;
+const CardTitle = styled.h1`
   font-size: 20px;
-  font-weight: 900;
+  font-weight: bold;
+  white-space: pre-wrap;
+  word-break: break-all;
 `;
 const CardInfo = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
   align-items: flex-start;
-  font-size: 20px;
+  height: 40px;
+  font-size: 14px;
   font-weight: bold;
+  color: #717171;
+`;
+const CardMetaData = styled.div`
+  display: flex;
+  justify-content: space-between;
+  font-size: 10px;
   color: #717171;
 `;
--- src/components/ReviewCard/index.tsx
+++ src/components/ReviewCard/index.tsx
@@ -1,7 +1,6 @@
 import { motion } from "framer-motion";
 import React from "react";
 import styled from "styled-components";
-import userImage from "@assets/userImage.svg";
 
 interface ReviewCardProps {
   imageUrl: string;
@@ -14,12 +13,13 @@ function ReviewCard(props: ReviewCardProps) {
   return (
     <CardContainer whileHover={{ scale: 1.01, opacity: 0.8 }}>
       <CardImage img_src={props.imageUrl} />
-      <CardTitle>{props.title}</CardTitle>
-      <CardPreview>{props.previewText}</CardPreview>
-      <AuthorInfo>
-        <img src={userImage} alt="userImg" style={{ marginRight: "3px" }} />
-        <div>작성자 : {props.user}</div>
-      </AuthorInfo>
+      <CardInfoBox>
+        <CardTitle>{props.title}</CardTitle>
+        <CardPreview>{props.previewText}</CardPreview>
+        <AuthorInfo>
+          <div>작성자 : {props.user}</div>
+        </AuthorInfo>
+      </CardInfoBox>
     </CardContainer>
   );
 }
@@ -31,32 +31,46 @@ const CardContainer = styled(motion.div)`
   flex-direction: column;
   justify-content: space-between;
   align-items: flex-start;
-  width: 580px;
-  height: 625px;
+  width: 100%;
+  height: 400px;
+  box-shadow: 0px 0px 20px rgba(0, 0, 0, 0.25);
+  border-radius: 5px;
+  cursor: pointer;
 `;
+
 const CardImage = styled.div<{ img_src: string }>`
   width: 100%;
-  height: 390px;
+  height: 220px;
   background-image: url(${(props) => props.img_src});
   background-position: center;
   background-repeat: no-repeat;
   background-size: cover;
   background-color: white;
+  border-radius: 5px 5px 0 0;
+`;
+const CardInfoBox = styled.div`
+  flex: 1;
+  width: 100%;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 `;
 const CardTitle = styled.h1`
-  font-size: 30px;
+  font-size: 20px;
   font-weight: bold;
   white-space: pre-wrap;
   word-break: break-all;
 `;
 const CardPreview = styled.p`
   color: #717171;
-  font-size: 20px;
+  font-size: 14px;
   line-height: 1.5;
   white-space: pre-wrap;
   word-break: break-all;
 `;
 const AuthorInfo = styled.div`
+  font-size: 10px;
   display: flex;
   align-items: center;
   color: #717171;
