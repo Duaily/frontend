c526007cdfc908e37ed4bcd67de07ce74608ef32
sangminlee98 <tkdals0975@naver.com>
2022-11-25 04:07:41 +0900

    style : 후기 상세 페이지 모바일 사이즈 반응형

--- src/components/Comment/index.tsx
+++ src/components/Comment/index.tsx
@@ -1,6 +1,6 @@
-import dayjs from "dayjs";
 import React from "react";
-import styled from "styled-components";
+import * as S from "./styles";
+import dayjs from "dayjs";
 
 interface CommentProps {
   author: string;
@@ -10,40 +10,14 @@ interface CommentProps {
 
 function Comment({ author, createdAt, content }: CommentProps) {
   return (
-    <Container>
-      <Info>
+    <S.Container>
+      <S.Info>
         <h1>{author}</h1>
         <p>{dayjs(createdAt).format("YYYY.MM.DD")}</p>
-      </Info>
-      <Content>{content}</Content>
-    </Container>
+      </S.Info>
+      <S.Content>{content}</S.Content>
+    </S.Container>
   );
 }
 
 export default Comment;
-
-const Container = styled.div`
-  width: 100%;
-  padding: 15px;
-  border-bottom: 1px solid #9a9a9a;
-`;
-const Info = styled.div`
-  display: flex;
-  align-items: center;
-  & > h1 {
-    font-size: 20px;
-    font-weight: 700;
-    line-height: 40px;
-    margin-right: 20px;
-  }
-  & > p {
-    font-size: 14px;
-    font-weight: 700;
-    line-height: 40px;
-    color: #9a9a9a;
-  }
-`;
-const Content = styled.p`
-  font-size: 20px;
-  line-height: 40px;
-`;
